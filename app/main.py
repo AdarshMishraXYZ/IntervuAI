b79dafe5457702f8fa3082dd63588fa23f0c343d
AdarshMishraXYZ <7890adarshmishra@gmail.com>
2026-06-03 13:34:10 +0530

    Enable CORS for frontend

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
-from app.api.v1.github import router as github_router
-from app.api.v1.resume import router as resume_router
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.v1.auth import router as auth_router
-from app.api.v1.health import router as health_router
-from app.api.v1.leetcode import router as leetcode_router
 from app.api.v1.candidate import router as candidate_router
+from app.api.v1.github import router as github_router
+from app.api.v1.health import router as health_router
 from app.api.v1.interview import router as interview_router
 from app.api.v1.interview_session import router as interview_session_router
+from app.api.v1.leetcode import router as leetcode_router
+from app.api.v1.resume import router as resume_router
 
 app = FastAPI(
     title="IntervuAI API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(
@@ -24,38 +37,43 @@ app.include_router(
     prefix="/api/v1/auth",
     tags=["Authentication"]
 )
+
 app.include_router(
     resume_router,
     prefix="/api/v1/resume",
     tags=["Resume"]
 )
+
 app.include_router(
     github_router,
     prefix="/api/v1/github",
     tags=["GitHub"]
-
 )
+
 app.include_router(
     leetcode_router,
     prefix="/api/v1/leetcode",
     tags=["LeetCode"]
-
 )
+
 app.include_router(
     candidate_router,
     prefix="/api/v1/candidate",
     tags=["Candidate"]
 )
+
 app.include_router(
     interview_router,
     prefix="/api/v1/interview",
     tags=["Interview"]
 )
+
 app.include_router(
     interview_session_router,
     prefix="/api/v1/interview-session",
     tags=["Interview Session"]
 )
+
 
 @app.get("/")
 def root():
